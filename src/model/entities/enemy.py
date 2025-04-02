b4978cc9e2518daa7c7e6adb05dfbe02be68e4e6
Eduardo Cavazin <eduardotorcav@gmail.com>
2025-04-02 08:26:19 -0300

    Checkpoint

--- src/model/entities/enemy.py
+++ src/model/entities/enemy.py
@@ -3,6 +3,6 @@ from src.model.entities.entity import Entity
 
 class Enemy(Entity):
     def __init__(self, id, position, size, speed, health, name, weapon, ammo, status):
+        super().__init__(id, position, size, speed, health, name, weapon, ammo, image, status)
         image = pygame.image.load('assets\sprites\enemy.png') 
         image = pygame.transform.scale(image, size)
-        super().__init__(id, position, size, speed, health, name, weapon, ammo, image, status)
